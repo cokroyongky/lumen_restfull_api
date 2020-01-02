9556720244fce71c5635a55f806b3e0fcc00c084
Prof Yongky <cokro.yongky@koltiva.com>
2020-01-02 14:31:13 +0700

    Fix encrypt using bycript

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,16 +4,20 @@
  * @Author: C Yongky Pranowo
  * @Date:   2019-11-30 23:32:03
  * @Last Modified by:   C Yongky Pranowo
- * @Last Modified time: 2019-11-30 23:32:27
+ * @Last Modified time: 2020-01-02 14:30:52
  */
 
 namespace App\Http\Controllers;
+
 use App\Models\MstUser;
 use Firebase\JWT\JWT;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Laravel\Lumen\Routing\Controller as BaseController;
-class AuthController extends BaseController 
+use Illuminate\Contracts\Encryption\DecryptException;
+use Illuminate\Support\Facades\Crypt;
+
+class AuthController extends BaseController
 {
     /**
      * The request instance.
@@ -27,7 +31,8 @@ class AuthController extends BaseController
      * @param  \Illuminate\Http\Request  $request
      * @return void
      */
-    public function __construct(Request $request) {
+    public function __construct(Request $request)
+    {
         $this->request = $request;
     }
     /**
@@ -36,25 +41,27 @@ class AuthController extends BaseController
      * @param  \App\MstUser   $MstUser
      * @return string
      */
-    protected function jwt(MstUser $MstUser) {
+    protected function jwt(MstUser $MstUser)
+    {
         $payload = [
             'iss' => "lumen-jwt", // Issuer of the token
             'sub' => $MstUser->id, // Subject of the token
             'iat' => time(), // Time when JWT was issued. 
-            'exp' => time() + 60*60 // Expiration time
+            'exp' => time() + 60 * 60 // Expiration time
         ];
-        
+
         // As you can see we are passing `JWT_SECRET` as the second parameter that will 
         // be used to decode the token in the future.
         return JWT::encode($payload, env('JWT_SECRET'));
-    } 
+    }
     /**
      * Authenticate a MstUser and return the token if the provided credentials are correct.
      * 
      * @param  \App\MstUser   $MstUser 
      * @return mixed
      */
-    public function authenticate(MstUser $MstUser) {
+    public function authenticate(MstUser $MstUser)
+    {
         $this->validate($this->request, [
             'user_email'     => 'required|email',
             'user_password'  => 'required'
@@ -70,7 +77,20 @@ class AuthController extends BaseController
                 'message' => 'user_email does not exist.'
             ], 400);
         }
-        
+
+        try {
+            $decrypted = Crypt::decrypt($MstUser->user_password);
+            if ($decrypted ==  $this->request->input('user_password')) {
+                return response()->json([
+                    'token' => $this->jwt($MstUser)
+                ], 200);
+            }
+        } catch (DecryptException $e) {
+            return response()->json([
+                'message' => 'user_email or user_password is wrong.'
+            ], 400);
+        }
+
         // Verify the user_password and generate the token
         if (Hash::check($this->request->input('user_password'), $MstUser->user_password)) {
             return response()->json([
